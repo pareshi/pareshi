cbb8e6c809f9ad06996e49c5506f2c171cbc72ae
pareshi-rajveer <pareshi.091997@gmail.com>
2020-10-07 18:07:49 +0530

    Custom cursor added

--- js/navigation.js
+++
@@ -1,8 +0,0 @@
-let header = document.getElementById('header');
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
-
-navBarToggle.addEventListener('click', function () {
-    mainNav.classList.toggle('active-menu');
-    header.classList.toggle('active-header');
-});
